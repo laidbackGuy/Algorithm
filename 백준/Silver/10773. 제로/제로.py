f771e6b35b4a011cca5698bdf527411e6aa23ea3
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2023-08-28 10:54:17 +0900

    [Silver IV] Title: 제로, Time: 152 ms, Memory: 118100 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+# 제로
+import sys
+input = sys.stdin.readline
+
+K = int(input())
+stack = []
+for i in range(K):
+    num = int(input())
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+print(sum(stack))
