7cc01f001f92ef5071966b4f937e616bcd4d21c8
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2023-11-08 20:32:23 +0900

    [Silver I] Title: 미로 탐색, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,25 @@
 # 미로 탐색
 def bfs():
-    queue = []
-    queue.append((0, 0))
+    queue = [(0, 0)]
     visited[0][0] = 1
     while queue:
-        # print('while')
         i, j = queue.pop(0)
-        if (i, j) == (N-1, M-1):
+        # print(i, j)
+        if i == N-1 and j == M-1:
+            # for row in visited:
+                # print(row)
             return visited[i][j]
-        for di, dj in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+        for di, dj in [[1, 0], [0, 1], [0, -1], [-1, 0]]:
             ni, nj = i + di, j + dj
-            # print(ni, nj)
-            if 0 <= ni < N and 0 <= nj < M and maze[ni][nj] == '1' and visited[ni][nj] == 0:
-                queue.append((ni, nj))
-                # print(queue)
-                visited[ni][nj] = visited[i][j] + 1
+            if 0 <= ni < N and 0 <= nj < M:
+                if arr[ni][nj] == '1' and not visited[ni][nj]:
+                    queue.append((ni, nj))
+                    visited[ni][nj] = visited[i][j] + 1
 
 
 N, M = map(int, input().split())
-maze = [list(input()) for _ in range(N)]
-# print(maze)
-visited = [[0] * M for _ in range(N)]
+arr = [input() for _ in range(N)]
+# print(arr)
+visited = [[0 for _ in range(M)]for _ in range(N)]
+# print(visited)
 print(bfs())
-# for row in visited:
-#     print(row)
