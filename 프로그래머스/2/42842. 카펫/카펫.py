0393e65dbf83f0d26c9172b621e9e82aff3e13f2
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-01-21 20:26:27 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,9 @@
+def solution(brown, yellow):
+    ans = 1
+    for i in range(1, (yellow // 2) + 1):
+        k = yellow / i
+        if brown - (2 * (i+2)) == k * 2:
+            ans = i
+            break
+    print(ans)
+    return [(yellow / ans) + 2, ans+2]
