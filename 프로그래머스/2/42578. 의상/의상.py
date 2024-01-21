1f5334dce7537cc84892c68e4857cbe7ec35fb38
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-01-21 22:07:27 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,9 @@
 def solution(clothes):
-    answer = 0
     my_dict = {}
     for item, category in clothes:
         my_dict.setdefault(category, [])
         my_dict[category].append(item)
-    # print(my_dict)
     answer = 1
-    for k, v in my_dict.items():
-        # print(v)
+    for v in my_dict.values():
         answer *= (len(v)  + 1)
     return answer - 1
