fe1d593d25e6e520d56e3280248db568deb6172c
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-01-24 22:45:49 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,32 +1,34 @@
 answer = 1e9
 flag = False
 def solution(begin, target, words):
-    global flag
     visited = [0] * len(words)
     
     def dfs(current, target, cnt):
-        global answer
-        global flag
-        if current == target:
-            if cnt < answer:
+        global answer, flag
+        
+        if current == target:   # target이 되었을 때 return
+            if cnt < answer:    # cnt 최솟값 찾기
                 answer = cnt
-            flag = True
+            flag = True         # target에 도달 가능하다고 flag 세우기
             return
         
-        if cnt >= answer:
+        if cnt >= answer:       # 가지치기(cnt가 기존 최솟값을 이미 넘어갔다면 return)
             return
         
         for i in range(len(words)):
-            if not visited[i]:
+            if not visited[i]:  # 방문한 적 없는 단어만 가기
                 same_char = 0
-                for k in range(len(begin)):
+                for k in range(len(begin)):         # 알파벳 한개만 다른 단어 찾기
                     if current[k] == words[i][k]:
                         same_char += 1
-                if same_char == len(begin) - 1:
-                    visited[i] = 1
-                    dfs(words[i], target, cnt + 1)
-                    visited[i] = 0
+                if same_char == len(begin) - 1: 
+                    visited[i] = 1                  # 방문 처리
+                    dfs(words[i], target, cnt + 1)  # 다음 재귀
+                    visited[i] = 0                  # 미방문 처리
+                    
     dfs(begin, target, 0)
+    
     if flag is False:
         return 0
+    
     return answer
