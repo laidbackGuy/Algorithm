ddcb0518a9fa82681e8fed7355ef46cde5701d1a
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-02-18 21:32:55 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -7,15 +7,12 @@ def solution(k, dungeons):
         global answer
         for i in range(len(dungeons)):
             if k >= dungeons[i][0] and visited[i] == 0:
-                visited[i] = 1
+                visited[i] = 1  # 방문처리
                 brute_force(k - dungeons[i][1], cnt + 1, visited)
-                visited[i] = 0
-        else:
-            if cnt > answer:
-                answer = cnt
-                # print(cnt)
-            return
-        
+                visited[i] = 0  # 미방문처리 
+        if cnt > answer:
+            answer = cnt
+
     brute_force(k, 0, visited)
     return answer
     
