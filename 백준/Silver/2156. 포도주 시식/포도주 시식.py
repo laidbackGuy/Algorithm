ab14475075a7c7c5e4e08ddfd3b980903f1e94e2
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-05-09 20:13:50 +0900

    [Silver I] Title: 포도주 시식, Time: 748 ms, Memory: 116208 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -15,9 +15,6 @@ if N > 2:
     dp[2] = max(arr[0]+arr[2], arr[1] + arr[2])
 if N > 3:
     for i in range(3, N):
-        dp[i] = max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i]
-
-    for i in range(3, N):
         dp[i] = max(max(dp[:i-1]), max(dp[:i-2]) + arr[i-1]) + arr[i]
 # print(dp)
 print(max(dp))
