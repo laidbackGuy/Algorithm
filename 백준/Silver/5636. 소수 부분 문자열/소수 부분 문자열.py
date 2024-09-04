51e4c8332a3f00b4c30edd7c857c48aca7579202
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-09-04 14:31:21 +0900

    [Silver I] Title: 소수 부분 문자열, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/5636. 소수 부분 문자열/소수 부분 문자열.py
@@ -0,0 +1,25 @@
+# 소수 부분 문자열
+
+import sys
+input = sys.stdin.readline
+
+while 1:
+    my_str = input().rstrip()
+    if my_str == '0':
+        break
+
+    if len(my_str) > 5:
+        max_range = 6
+    else:
+        max_range = len(my_str)
+
+    candi = 0
+    for ran in range(1, max_range):
+        for i in range(len(my_str)-ran):
+            now = my_str[i:i+ran]
+            for j in range(2, int(now)):
+                if int(now) % j == 0:
+                    break
+            else:
+                candi = max(int(now), candi)
+    print(candi)
