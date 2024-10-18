835955f7a4c42082941e84aaeb85f9339d5ffbc5
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-10-18 10:29:50 +0900

    [level 2] Title: 노선별 평균 역 사이 거리 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284531. 노선별 평균 역 사이 거리 조회하기/노선별 평균 역 사이 거리 조회하기.sql
+++ 프로그래머스/2/284531. 노선별 평균 역 사이 거리 조회하기/노선별 평균 역 사이 거리 조회하기.sql
@@ -2,3 +2,6 @@
 select ROUTE, concat(round(sum(d_between_dist), 1), 'km') as TOTAL_DISTANCE, concat(round(avg(d_between_dist), 2), 'km') as AVERAGE_DISTANCE from subway_distance
 group by route
 order by sum(d_between_dist) desc
+# ORDER BY TOTAL_DISTANCE 로 했다가 틀렸는데,
+# TOTAL_DISTANCE는 소수점 반올림으로 한 번 가공한 데이터이기 때문에 
+# TOTAL_DISTANCE를 기준으로 정렬하면 안되는 것임.
