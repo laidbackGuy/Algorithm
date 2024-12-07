6db86a47d1435212de71bcdd522de328827fb762
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2024-12-07 23:07:12 +0900

    [Silver II] Title: 예산, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -6,33 +6,24 @@ N = int(input())
 orders = list(map(int, input().split()))
 budget = int(input())
 
-total = sum(orders)
 right = max(orders)
-if total <= budget:
-    print(right)
-else:
-    left = 1
-    answer = 0
-    while left <= right:
-        mid = (left + right) // 2
-        cur = 0
-        flag = True
-        # print(left, mid, right)
-        # print('mid:', mid)
-        for order in orders:
-            if order <= mid:
-                cur += order
-            else:
-                cur += mid
-            if cur > budget:
-                flag = False
-                # print('cur:', cur)
-                break
-        if not flag:
-            # print('배정 실패')
-            right = mid - 1
+left = 1
+answer = 0
+while left <= right:
+    mid = (left + right) // 2
+    cur = 0
+    flag = True
+    for order in orders:
+        if order <= mid:
+            cur += order
         else:
-            # print('배정 성공')
-            answer = max(answer, mid)
-            left = mid + 1
-    print(answer)
+            cur += mid
+        if cur > budget:
+            flag = False
+            break
+    if not flag:
+        right = mid - 1
+    else:
+        answer = max(answer, mid)
+        left = mid + 1
+print(answer)
