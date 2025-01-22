39cf08da860afe5d0ba579a9c86e6b091f4232ae
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-01-22 20:32:24 +0900

    [Silver II] Title: KCPC, Time: 128 ms, Memory: 111476 KB -BaekjoonHub

---
+++ 백준/Silver/3758. KCPC/KCPC.py
@@ -0,0 +1,37 @@
+# KCPC
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for tc in range(T):
+    rank = 1
+    n, k, t, m = map(int, input().split())
+    table = [[]] + [[0] * (k+1) for _ in range(n)]
+    submit = [0] * (n+1)
+    last_submit = [0] * (n+1)
+    for time in range(m):
+        i, j, s = map(int, input().split())
+        table[i][j] = max(table[i][j], s)
+        submit[i] += 1
+        last_submit[i] = time
+    # print(table)
+    # print(submit)
+    # print(last_submit)
+    score = sum(table[t])
+    # print(score)
+    for i in range(1, n+1):
+        if i == t:
+            continue
+        now = sum(table[i])
+        if now > score:
+            rank += 1
+            continue
+        if now == score:
+            if submit[i] < submit[t]:
+                rank += 1
+                continue
+            elif submit[i] == submit[t]:
+                if last_submit[i] < last_submit[t]:
+                    rank += 1
+                    continue
+    print(rank)
