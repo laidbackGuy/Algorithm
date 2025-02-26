4e9f1d66704097fd3c461236763563680f411c7d
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-02-26 20:40:53 +0900

    [Silver III] Title: 블로그, Time: 148 ms, Memory: 58224 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,5 +1,6 @@
 # 블로그
 import sys
+
 input = sys.stdin.readline
 
 N, X = map(int, input().split())
@@ -10,16 +11,16 @@ right = X
 cnt = 1
 max_v = now
 
-for _ in range(X, N):
+for i in range(X, N):
     now -= arr[left]
-    now += arr[right]
+    now += arr[i]
     if now > max_v:
         max_v = now
         cnt = 1
     elif now == max_v:
         cnt += 1
     left += 1
-    right += 1
+
 if max_v:
     print(max_v)
     print(cnt)
