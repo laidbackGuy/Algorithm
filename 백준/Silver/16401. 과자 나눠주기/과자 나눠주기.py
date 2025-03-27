e35b307577a4ac7298fdd303a5179b62f8afdc82
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-03-27 21:01:33 +0900

    [Silver II] Title: 과자 나눠주기, Time: 2536 ms, Memory: 145152 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,44 @@
+# 과자 나눠주기
+import sys
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+snacks = list(map(int, input().split()))
+left, right = 1, max(snacks)
+
+
+def check(length):
+    global answer
+    cnt = 0
+    # print(length)
+    for s in snacks:
+        while 1:
+            if s >= length:
+                # print('s', s)
+                s -= length
+                cnt += 1
+                # print('cnt', cnt)
+                if cnt == M:
+                    answer = length
+                    return True
+            else:
+                break
+    return False
+
+
+flag = False
+answer = 1
+while left <= right:
+    mid = (left + right) // 2
+    # print(left, mid, right)
+    if check(mid):
+        flag = True
+        left = mid + 1
+    else:
+        right = mid - 1
+    # print(left, mid, right)
+    # print()
+if not flag:
+    print(0)
+else:
+    print(answer)
