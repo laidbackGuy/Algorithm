b075b32e72ca80c2a9f706309166a11c4da996aa
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-04-09 21:03:29 +0900

    [Gold IV] Title: 돌 그룹, Time: 528 ms, Memory: 50112 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.py
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.py
@@ -10,52 +10,29 @@ if total % 3 != 0:
     print(answer)
 else:
     share = total // 3
-    arr = [A, B, C]
-    arr.sort()
     visited = [[0] * (total + 1) for _ in range(total + 1)]
-    q = deque([(arr[0], arr[1])])
-    visited[arr[0]][arr[1]] = 1
+    q = deque([(A, B)])
+    visited[A][B] = 1
     while q:
         a, b = q.popleft()
         c = total - a - b
-        # print(a, b, c)
         if a == b == c:
             answer = 1
             break
-        if b != c:
-            now = []
-            if b < c:
-                now.append(b * 2)
-                now.append(c - b)
+        for x, y in (a, b), (b, c), (a, c):
+            if x > y:
+                x -= y
+                y += y
+            elif y > x:
+                y -= x
+                x += x
             else:
-                now.append(c * 2)
-                now.append(b - c)
-            now.sort()
-            if visited[now[0]][now[1]] == 0:
-                q.append(now)
-                visited[now[0]][now[1]] = 1
-        if a != c:
-            now = []
-            if a < c:
-                now.append(a * 2)
-                now.append(c - a)
-            else:
-                now.append(c * 2)
-                now.append(a - c)
-            now.sort()
-            if visited[now[0]][now[1]] == 0:
-                q.append(now)
-                visited[now[0]][now[1]] = 1
-        if a != b:
-            now = []
-            if a < b:
-                now.append(a * 2)
-                now.append(b - a)
-            else:
-                now.append(b * 2)
-                now.append(a - b)
-            now.sort()
-            if visited[now[0]][now[1]] == 0:
-                q.append(now)
-                visited[now[0]][now[1]] = 1
+                continue
+            z = total - x - y
+            a = min(x, y, z)
+            b = max(x, y, z)
+            if visited[a][b] == 0:
+                q.append((a, b))
+                visited[a][b] = 1
+
     print(answer)
