bbbdd1b6f9dfa1b930dbee6049a658f296a48ec0
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-06-25 22:27:45 +0900

    [Gold V] Title: 동전, Time: 128 ms, Memory: 36420 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,23 @@
+# 동전
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    coins = [0] + list(map(int, input().split()))
+    M = int(input())
+    dp = [[0] * (M+1) for _ in range(N+1)]
+    
+    for i in range(1, N+1):
+        dp[i][0] = 1
+
+    for i in range(1, N+1):
+        now = coins[i]
+        for j in range(1, M+1):
+            dp[i][j] = dp[i-1][j]
+
+            if j >= now:
+                dp[i][j] += dp[i][j-now]
+
+    print(dp[N][M])
