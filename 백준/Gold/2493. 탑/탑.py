2a629308f6b13b0c507d761b7ee64b5cb377b18f
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-07-08 22:28:10 +0900

    [Gold V] Title: 탑, Time: 648 ms, Memory: 110696 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,29 +1,18 @@
-# 탑
-import sys, heapq
-input = sys.stdin.readline
-
 N = int(input())
 if N == 1:
     print(0)
 else:
     towers = list(map(int, input().split()))
     table = [0] * N
-    pq = []
-    heapq.heapify(pq)
-    heapq.heappush(pq, (towers[N-1], N-1))
+    stack = [(towers[N-1], N-1)]
     for i in range(N-2, -1, -1):
         height = towers[i]
-        # print(i+1, height)
-        while pq:
-            now, idx = pq[0]
-            # print(now, idx)
+        while stack:
+            now, idx = stack[-1]
             if now <= height:
-                heapq.heappop(pq)
+                stack.pop()
                 table[idx] = i + 1
-                # print(table)
             else:
                 break
-        heapq.heappush(pq, (towers[i], i))
-        # print(pq)
-        # print()
+        stack.append((towers[i], i))
     print(*table)
