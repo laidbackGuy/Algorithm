67039fd86d39c65e4aa409b52958cc21e6482265
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-08-06 21:39:55 +0900

    [Gold V] Title: 빗물, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,23 +1,37 @@
 # 빗물
+import sys
+input = sys.stdin.readline
+
 H, W = map(int, input().split())
-heights = list(map(int, input().split()))
-arr = [[0] * W for _ in range(H)]
+h = list(map(int, input().split()))
 
-for i in range(len(heights)):
-    for floor in range(heights[i]):
-        arr[floor][i] = 1
+if W <= 2:
+    print(0)
+else:
+    answer = 0
 
-ans = 0
-for i in range(H):
-    cnt = 0
-    state = False
-    for j in range(W):
-        if arr[i][j]:
-            ans += cnt
-            cnt = 0
-            state = True
+    cur_max = h[0]
+    stack = []
+    for i in range(1, W):
+        now = h[i]
+        if now >= cur_max:
+            for deep in stack:
+                answer += (cur_max - deep)
+            stack = []
+            cur_max = now
         else:
-            if state:
-                cnt += 1
-print(ans)
+            stack.append(now)
 
+    cur_max = h[W-1]
+    stack = []
+    for i in range(W-2, -1, -1):
+        now = h[i]
+        if now > cur_max:
+            for deep in stack:
+                answer += (cur_max - deep)
+            stack = []
+            cur_max = now
+        else:
+            stack.append(now)
+
+    print(answer)
