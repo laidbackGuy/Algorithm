29efccf122f571ffab6f36f4a842ef81f18d4e2e
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-09-20 01:05:40 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,21 +3,23 @@ class Solution {
         int answer = 0;
         boolean[] visited = new boolean[n];
         
-        for (int i = 0 ; i < n ; i++){
+        for(int i=0 ; i < n ; i++){
             if (!visited[i]){
                 dfs(computers, visited, i, n);
                 answer++;
             }
         }
+        
+        
         return answer;
     }
     
-    private void dfs(int[][] computers, boolean[] visited, int node, int l){
+    private void dfs(int[][] computers, boolean[] visited, int node, int length){
         visited[node] = true;
         
-        for(int i = 0 ; i < l ; i++){
-            if(computers[node][i] == 1 && !visited[i]){
-                dfs(computers, visited, i, l);
+        for(int i=0; i < length ; i++){
+            if (computers[node][i] == 1 && !visited[i]){
+                dfs(computers, visited, i, length);
             }
         }
     }
