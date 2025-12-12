b29bf1703da906f693d8b4f4db37fec0aa513da3
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-12-13 03:28:52 +0900

    [Silver I] Title: 회전 초밥, Time: 92 ms, Memory: 35548 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -7,26 +7,57 @@ N, d, k, c = map(int, input().split())
 sushi = [int(input()) for _ in range(N)]
 
 answer = 0
+sushi_cnt = [0] * (d + 1)
+cur_unique_sushi = 0
 
-q = deque(sushi[:k])
-my_set = set(q)
-my_set.add(c)
-answer = max(len(my_set), answer)
-
-for i in range(k, N):
+for i in range(k):
     now = sushi[i]
-    q.popleft()
-    q.append(now)
-    my_set = set(q)
-    my_set.add(c)
-    answer = max(len(my_set), answer)
+    if sushi_cnt[now] == 0:
+        cur_unique_sushi += 1
+        if sushi_cnt[c] == 0:
+            answer = max(answer, cur_unique_sushi + 1)
+        else:
+            answer = max(answer, cur_unique_sushi)
+    sushi_cnt[now] += 1
 
-for i in range(k-1):
-    now = sushi[i]
-    q.popleft()
-    q.append(now)
-    my_set = set(q)
-    my_set.add(c)
-    answer = max(len(my_set), answer)
+for i in range(N):
+    new = sushi[(i+k) % N]
+    out = sushi[i]
+    if sushi_cnt[new] == 0:
+        cur_unique_sushi += 1
+    sushi_cnt[new] += 1
+    sushi_cnt[out] -= 1
+    if sushi_cnt[out] == 0:
+        cur_unique_sushi -= 1
+    if sushi_cnt[c] == 0:
+        answer = max(answer, cur_unique_sushi + 1)
+    else:
+        answer = max(answer, cur_unique_sushi)
 
 print(answer)
+
+# set과 max 이용
+# answer = 0
+#
+# q = deque(sushi[:k])
+# my_set = set(q)
+# my_set.add(c)
+# answer = max(len(my_set), answer)
+#
+# for i in range(k, N):
+#     now = sushi[i]
+#     q.popleft()
+#     q.append(now)
+#     my_set = set(q)
+#     my_set.add(c)
+#     answer = max(len(my_set), answer)
+#
+# for i in range(k-1):
+#     now = sushi[i]
+#     q.popleft()
+#     q.append(now)
+#     my_set = set(q)
+#     my_set.add(c)
+#     answer = max(len(my_set), answer)
+#
+# print(answer)
