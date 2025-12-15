3a22edde736862f134605ed934e61ad755407932
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2025-12-15 19:27:28 +0900

    [Silver II] Title: DNA 비밀번호, Time: 424 ms, Memory: 34344 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -6,21 +6,14 @@ S, P = map(int, input().split())
 dna_str = input().rstrip()
 A, C, G, T = map(int, input().split())
 
+my_dict = {'A': 0, 'C': 1, 'G':2, 'T':3}
 answer = 0
-
 check = [0] * 4
 
 for i in range(P):
     now = dna_str[i]
-    if now == 'A':
-        check[0] += 1
-    elif now == 'C':
-        check[1] += 1
-    elif now == 'G':
-        check[2] += 1
-    elif now == 'T':
-        check[3] += 1
-# print(check)
+    check[my_dict[now]] += 1
+
 if check[0] >= A and check[1] >= C and check[2] >= G and check[3] >= T:
     answer += 1
 
@@ -28,26 +21,10 @@ for i in range(P, S):
     new = dna_str[i]
     out = dna_str[i-P]
 
-    if out == 'A':
-        check[0] -= 1
-    elif out == 'C':
-        check[1] -= 1
-    elif out == 'G':
-        check[2] -= 1
-    elif out == 'T':
-        check[3] -= 1
+    check[my_dict[out]] -= 1
+    check[my_dict[new]] += 1
 
-    if new == 'A':
-        check[0] += 1
-    elif new == 'C':
-        check[1] += 1
-    elif new == 'G':
-        check[2] += 1
-    elif new == 'T':
-        check[3] += 1
-    # print(check)
     if check[0] >= A and check[1] >= C and check[2] >= G and check[3] >= T:
-        # print(i, check)
         answer += 1
 
 print(answer)
