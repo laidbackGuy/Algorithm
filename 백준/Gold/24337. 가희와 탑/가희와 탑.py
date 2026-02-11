0c9857d24bccfc4d6b99dd1f9a3994ca8df25eff
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2026-02-11 22:00:52 +0900

    [Gold III] Title: 가희와 탑, Time: 124 ms, Memory: 36640 KB -BaekjoonHub

---
+++ 백준/Gold/24337. 가희와 탑/가희와 탑.py
@@ -0,0 +1,36 @@
+# 가희와 탑
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, a, b = map(int, input().split())
+if N == 1:
+    print(1)
+elif a + b > N + 1:
+    print(-1)
+else:
+    buildings = deque([])
+    remain = 0
+
+    if a + b < N + 1:
+        if a == 1:
+            for i in range((N + 1) - (a + b)):
+                buildings.appendleft(1)
+        else:
+            remain = (N + 1) - (a + b)
+
+    buildings.appendleft(max(a, b))
+
+    for floor in range(a-1, 0, -1):
+        buildings.appendleft(floor)
+
+    for floor in range(b-1, 0, -1):
+        buildings.append(floor)
+
+    for i in range(remain):
+        buildings.appendleft(1)
+
+    # print(buildings)
+
+    for n in buildings:
+        print(n, end=" ")
