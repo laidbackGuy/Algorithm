f3fd1dbb9f4eedd9711a8ad86dd2a2772834d3bc
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2026-05-07 21:55:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 32.7 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,7 @@
 function solution(s) {
-    // const arr = s.split(' ').map(Number);
-    const arr = s.split(' ');
-    return Math.min(...arr) + ' ' + Math.max(...arr);
+    const arr = s.split(' ').map(Number);
+    // const arr = s.split(' ');
+    arr.sort((a, b) => a - b);
+    return arr[0] + ' ' + arr[arr.length - 1];
+    // return Math.min(...arr) + ' ' + Math.max(...arr);
 }
