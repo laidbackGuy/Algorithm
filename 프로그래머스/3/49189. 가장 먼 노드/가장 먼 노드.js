3e7caf3e0bd4aabe9ff8f6606cf50bac54e3756c
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2026-05-07 23:20:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 491.56 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,45 +1,33 @@
-function solution(n, edge) {
-    var answer = 0;
-    const adj = Array.from({length:n+1}, () => Array(n+1).fill(0));
-    for(const [u, v] of edge){
-        adj[u][v] = 1;
-        adj[v][u] = 1;
-    }
-    
-    let farest = 0;
-    let cnt = 0;
-    
-    const visited = Array.from({length:n+1}, () => [false, 0]);
-    
-    const q = [];
-    let head = 0;
-    
-    q.push(1);
-    visited[1][0] = true, visited[1][1] = 0;
-    console.log()
-    while(head < q.length){
-        const now = q[head++];
-        
-        for(let next = 1; next<n+1; next++){
-            if (adj[now][next] === 0){
-                continue;
-            }
-            if (visited[next][0] === true){
-                continue;
-            }
-            visited[next][0] = true;
-            visited[next][1] = visited[now][1] + 1;
-            if (visited[next][1] > farest){
-                farest = visited[next][1];
-                cnt = 1;
-            }
-            else if (visited[next][1] === farest){
-                cnt++;
-            }
-            q.push(next);
+function solution(n, edges) {
+    // make adjacent list
+    const adjList = edges.reduce((G, [from, to]) => {
+        G[from] = (G[from] || []).concat(to);
+        G[to] = (G[to] || []).concat(from);
+        return G;
+    }, {});
+
+    // do BFS
+    const queue = [1];
+    const visited = {1: true};
+    const dist = {1: 0};
+    while(queue.length > 0) {
+        const node = queue.shift();
+
+        if (adjList[node]) {
+            adjList[node].forEach(n => {
+                if (!visited[n]) {
+                    queue.push(n);
+                    visited[n] = true;
+                    const d = dist[node] + 1;
+                    if (dist[n] == undefined || d < dist[n]) {
+                        dist[n] = d;
+                    }
+                }
+            });
         }
     }
-    
-    // console.log(visited);
-    return cnt;
+
+    const dists = Object.values(dist);
+    const maxDist = Math.max(...dists);
+    return dists.filter(d => d == maxDist).length;
 }
