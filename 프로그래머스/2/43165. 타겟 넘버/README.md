2859cf69e129095bda7f1ba53c74482a2be1036c
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2026-05-09 00:10:00 +0900

    [level 2] Title: 타겟 넘버, Time: 49.37 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 35.8 MB, 시간: 7.71 ms
+메모리: 37.2 MB, 시간: 49.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 07일 23:51:53
+2026년 05월 09일 00:09:56
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,21 @@
 function solution(numbers, target) {
-    var answer = 0;
-    
-    const rec = (cur, cnt) => {
-        if (cnt === numbers.length){
-            if (cur === target){
+    let answer = 0;
+
+    const stack = [[0, 0]]; // [현재합, 인덱스]
+
+    while (stack.length) {
+        const [cur, cnt] = stack.pop();
+
+        if (cnt === numbers.length) {
+            if (cur === target) {
                 answer++;
             }
-            return;
+            continue;
         }
-        rec(cur - numbers[cnt], cnt + 1);
-        rec(cur + numbers[cnt], cnt + 1);
+
+        stack.push([cur - numbers[cnt], cnt + 1]);
+        stack.push([cur + numbers[cnt], cnt + 1]);
     }
-    
-    rec(0, 0);
-    
+
     return answer;
 }
