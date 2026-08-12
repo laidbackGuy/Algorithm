012890e8c3a5f63f8cfdc3f6818d0474fc95e2d5
laidbackGuy <139303875+laidbackGuy@users.noreply.github.com>
2026-08-12 17:45:08 +0900

    [level 3] Title: 부대복귀, Time: 261.47 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -2,9 +2,7 @@ from collections import deque
 
 def solution(n, roads, sources, destination):
     answer = []
-    distance = [0] * (n+1)
     adj = [[] for _ in range(n+1)]
-    # cnt = 0
     visited = [-1] * (n+1)
     
     for u, v in roads:
@@ -25,6 +23,7 @@ def solution(n, roads, sources, destination):
     
     
     bfs()
+    
     for s in sources:
         answer.append(visited[s])
     return answer
